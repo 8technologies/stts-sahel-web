c6046694e5156310b6f46db748e9768def88783a
PriscillaCole <51949337+PriscillaCole@users.noreply.github.com>
2023-07-12 16:09:55 +0300

    lab-tests

--- app/Admin/Controllers/SeedLabController.php
+++ app/Admin/Controllers/SeedLabController.php
@@ -32,41 +32,32 @@ class SeedLabController extends AdminController
     protected function grid()
     {
         $grid = new Grid(new SeedLab());
+        $user = Admin::user();
+        //check the status of the form before displaying it
+        $grid->model()->where('status', '=', 'lab test assigned');
 
-        //ensure that a user only sees what he is supposed to
-        if (!auth('admin')->user()->isRole('commissioner')) {
+        //disable create button
+        $grid->disableCreateButton();
 
-            if (!auth('admin')->user()->isRole('inspector')) {
-                $grid->model()->where('applicant_id', auth('admin')->user()->id);
-            } else {
-                $grid->model()->where('inspector_id', auth('admin')->user()->id);
-            }
+        //disable edit for all users apart from lab technician
+        if(!$user->isRole('lab_technician')){
+            $grid->actions(function ($actions) {
+                $actions->disableDelete();
+                $actions->disableEdit();
+                
+            });
         }
-
-        //show an empty table if the status is not lab inspection assigned
-        
-      
 
         $grid->column('id', __('Id'));
         $grid->column('sample_request_number', __('Sample request number'));
         $grid->column('applicant_id', __('Applicant id'));
-        $grid->column('load_stock_number', __('Load stock number'));
-        $grid->column('sample_request_date', __('Sample request date'));
-        $grid->column('proof_of_payment', __('Proof of payment'));
         $grid->column('applicant_remarks', __('Applicant remarks'));
         $grid->column('seed_lab_test_report_number', __('Seed lab test report number'));
         $grid->column('seed_sample_request_number', __('Seed sample request number'));
-        $grid->column('seed_sample_size', __('Seed sample size'));
-        $grid->column('testing_methods', __('Testing methods'));
         $grid->column('germination_test_results', __('Germination test results'));
-        $grid->column('purity_test_results', __('Purity test results'));
-        $grid->column('moisture_content_test_results', __('Moisture content test results'));
-        $grid->column('additional_tests_results', __('Additional tests results'));
+        $grid->column('purity_test_results', __('Purity test results'));;
         $grid->column('test_decision', __('Test decision'));
-        $grid->column('reporting_and_signature', __('Reporting and signature'));
-        $grid->column('created_at', __('Created at'));
-        $grid->column('updated_at', __('Updated at'));
-
+  
         return $grid;
     }
 
@@ -119,24 +110,28 @@ class SeedLabController extends AdminController
         
              
         if($form->isEditing()){
-            $form_id= request()->route()->parameters()['seed-lab'];
-            $form = SeedLab::find($form_id);
-            $crop_declaration = LoadStock::where('id', $form->load_stock_id)->where('applicant_id', $form->applicant_id )->value('crop_declaration_id');
+           
+            $form_id= request()->route()->parameters()['seed_lab_test'];
+            $seed_lab = SeedLab::find($form_id);
+           
+            $crop_declaration = LoadStock::where('id', $seed_lab->load_stock_id)->where('applicant_id', $seed_lab->applicant_id )->value('crop_declaration_id');
             //get crop variety from crop_declaration id
             $crop_variety_id = CropDeclaration::where('id', $crop_declaration)->value('crop_variety_id');
             //get crop variety name from crop_variety id
-            $crop_variety= CropVariety::where('id', $crop_variety_id);
+            $crop_variety= CropVariety::where('id', $crop_variety_id)->first();
             //get crop name from crop variety
-            $crop_name = Crop::where('id', $crop_variety->crop_id)->value('name');
+            $crop_name = Crop::where('id', $crop_variety->crop_id)->value('crop_name');
 
-            
-            $form->text('applicant_id', __('Applicant name'));
-            $form->text('load_stock_id', __('Load stock number'))->readonly();
-            $form->display('', __('Crop'))->default($crop_name);
-            $form->select('priority', __('Priority'))->options(['low' => 'Low', 'medium' => 'Medium', 'high' => 'High']);
+            $applicant_name = Administrator::where('id', $seed_lab->applicant_id)->value('name');
+
+     
+                $form->display('', __('Applicant name'))->default($applicant_name);
+                $form->display('load_stock_id', __('Load stock number'))->readonly();
+                $form->display('', __('Crop'))->default($crop_name);
+                $form->display('', __('Variety'))->default($crop_variety->crop_variety_name);
+                $form->display('', __('Generation'))->default($crop_variety->crop_variety_generation);
         
-
-        $form->text('seed_lab_test_report_number', __('Seed lab test report number'));
+        $form->text('seed_lab_test_report_number', __('Seed lab test report number'))->default('labtest'. "/". mt_rand(10000000, 99999999));
         $form->text('seed_sample_request_number', __('Seed sample request number'));
         $form->decimal('seed_sample_size', __('Seed sample size'));
         $form->text('testing_methods', __('Testing methods'));
@@ -144,7 +139,11 @@ class SeedLabController extends AdminController
         $form->decimal('purity_test_results', __('Purity test results'));
         $form->decimal('moisture_content_test_results', __('Moisture content test results'));
         $form->textarea('additional_tests_results', __('Additional tests results'));
-        $form->text('test_decision', __('Test decision'));
+        $form->radio('test_decision', __('Test decision'))
+        ->options(['marketable' => 'Marketable', 'not marketable' => 'Not Marketable'])
+        ->when('marketable', function (Form $form) use ($crop_variety) {
+            $form->textarea('lot_number', __('Lot number'))->default($crop_variety->crop_variety_name. "/". mt_rand(10000000, 99999999));
+        });
         $form->textarea('reporting_and_signature', __('Reporting and signature'));
     }
         return $form;
