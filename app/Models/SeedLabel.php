37243de393fe19904d70b521dbbd1af1be4bdddd
PriscillaCole <51949337+PriscillaCole@users.noreply.github.com>
2023-11-28 13:14:59 +0300

    preorder

--- app/Models/SeedLabel.php
+++ app/Models/SeedLabel.php
@@ -70,7 +70,7 @@ class SeedLabel extends Model
                     // Update the quantity in the load stock table
                     $load_stock_id = SeedLab::find($model->seed_lab_id)->first()->load_stock_id;
                     $load_stock = LoadStock::find($load_stock_id);
-                    $load_stock->yield_quantity = $total_quantity;
+                    $load_stock->yield_quantity = 6000;
                     $load_stock->save();
 
                     // $marketable_seed = new MarketableSeed();
