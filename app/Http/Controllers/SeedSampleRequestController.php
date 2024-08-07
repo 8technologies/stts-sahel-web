7bf65ea5d8243121ec20e7b4c7dd79b3f06a7bec
PriscillaCole <51949337+PriscillaCole@users.noreply.github.com>
2024-08-07 14:35:47 +0300

    seed sample

--- app/Http/Controllers/SeedSampleRequestController.php
+++ app/Http/Controllers/SeedSampleRequestController.php
@@ -9,6 +9,8 @@ use App\Models\User;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use App\Models\Utils;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 
 
 class SeedSampleRequestController extends Controller
@@ -21,8 +23,27 @@ class SeedSampleRequestController extends Controller
 
     public function store(Request $request)
     {
-        $user = auth('api')->user();
-        $data = $request->all();
+      
+        $rules = [
+            'sample_request_number' => 'required',
+            'user_id' => 'required',
+            'load_stock_id' => 'required|exists:load_stocks,id',
+            'crop_variety_id' => 'required|exists:crop_varieties,id',
+            'sample_request_date' => 'required',
+            'quantity' => 'required|numeric',
+         
+        ];
+
+        try {
+            // Validate the incoming request data
+            $validatedData = Validator::make($request->all(), $rules)->validate();
+            
+        } catch (ValidationException $e) {
+            return response()->json([
+                'message' => 'Validation failed',
+                'errors' => $e->errors()
+            ], 422);
+        }
 
         if ($request->has('proof_of_payment')) 
        {
@@ -34,10 +55,10 @@ class SeedSampleRequestController extends Controller
             $photoPath = 'images/' . uniqid() . '.jpg'; 
             Storage::disk('admin')->put($photoPath, $photoData);
             
-            $data['proof_of_payment'] = $photoPath;
+            $validatedData ['proof_of_payment'] = $photoPath;
         }
 
-        $seedSample = SeedLab::create($data);
+        $seedSample = SeedLab::create( $validatedData );
         return Utils::apiSuccess($seedSample, 'Seed Sample Request submitted successfully.');
     }
 
@@ -52,7 +73,26 @@ class SeedSampleRequestController extends Controller
     {
         $seedSample = SeedLab::findOrFail($id);
 
-        $data = $request->all();
+        $rules = [
+            'sample_request_number' => 'required',
+            'user_id' => 'required',
+            'load_stock_id' => 'required|exists:load_stocks,id',
+            'crop_variety_id' => 'required|exists:crop_varieties,id',
+            'sample_request_date' => 'required',
+            'quantity' => 'required|numeric',
+         
+        ];
+
+        try {
+            // Validate the incoming request data
+            $validatedData = Validator::make($request->all(), $rules)->validate();
+            
+        } catch (ValidationException $e) {
+            return response()->json([
+                'message' => 'Validation failed',
+                'errors' => $e->errors()
+            ], 422);
+        }
 
         if ($request->has('proof_of_payment')) 
         {
@@ -64,9 +104,9 @@ class SeedSampleRequestController extends Controller
              $photoPath = 'images/' . uniqid() . '.jpg'; 
              Storage::disk('admin')->put($photoPath, $photoData);
              
-             $data['proof_of_payment'] = $photoPath;
+             $validatedData['proof_of_payment'] = $photoPath;
          }
-        $seedSample->update($data);
+        $seedSample->update($validatedData);
         return Utils::apiSuccess($seedSample, 'Seed Sample Request edited successfully.');
     }
 
@@ -77,15 +117,15 @@ class SeedSampleRequestController extends Controller
         return Utils::apiSuccess($seedSample, 'Seed Sample Request deleted successfully.');
     }
 
-       //get the inspections assigned to an inspector
-       public function getAssignedRequests($id)
-       {
+    //get the inspections assigned to an inspector
+    public function getAssignedRequests($id)
+    {
     
-           $SeedLabs = SeedLab::where('inspector_id', $id)->where('status', 'inspector assigned')->get();
-           $result = [];
-           //get the user name for each seed sample request
-              foreach ($SeedLabs as $SeedLab) 
-              {
+        $SeedLabs = SeedLab::where('inspector_id', $id)->where('status', 'inspector assigned')->get();
+        $result = [];
+        //get the user name for each seed sample request
+            foreach ($SeedLabs as $SeedLab) 
+            {
                 $user = User::find($SeedLab->user_id);
                 $SeedLab->user_name = $user->name;
                 $loadStock = LoadStock::where('id', $SeedLab->load_stock_id)->first();
@@ -95,30 +135,30 @@ class SeedSampleRequestController extends Controller
                     'user' => $user,
                     'loadStock' => $loadStock,
                 ];
-              }
-           return Utils::apiSuccess($result);
-       }
+            }
+        return Utils::apiSuccess($result);
+    }
    
-       //edit the inspections of an inspector
-       public function updateAssignedRequests(Request $request, $id)
-       {
-           $SeedLab = SeedLab::find($id);
-           // Check if the seed sample request exists
-           if (!$SeedLab) {
-               return Utils::apiError('seed sample request not found.', 404);
-           }
-   
-           $data = $request->all();
+    //edit the inspections of an inspector
+    public function updateAssignedRequests(Request $request, $id)
+    {
+        $SeedLab = SeedLab::find($id);
+        // Check if the seed sample request exists
+        if (!$SeedLab) {
+            return Utils::apiError('seed sample request not found.', 404);
+        }
 
-           //check the status of the seed sample request
+        $data = $request->all();
+
+        //check the status of the seed sample request
             if ($SeedLab->status == 'lab test assigned') {
                 //if its approved, then update the load stock table quantity column with the quantity of the seed sample request
                 $loadStock = LoadStock::where('id', $SeedLab->load_stock_id)->first();
                 $loadStock->yield_quantity = $data['validated_stock'];
                 $loadStock->save();
 
-              }
-           $SeedLab->update($data);
-           return Utils::apiSuccess($SeedLab, 'seed sample request form edited successfully.');
-       }
+            }
+        $SeedLab->update($data);
+        return Utils::apiSuccess($SeedLab, 'seed sample request form edited successfully.');
+    }
 }
