21ab2d2a9088a5c64acf0f850b88bbfea0392e57
HildahN <98084446+HildahN@users.noreply.github.com>
2024-10-22 12:33:03 +0300

    deleting the agro dealer
    Handle the AgroDealer "deleted" event and changes ther users role to basic user and deletes the notifications.

--- app/Observers/AgroDealerObserver.php
+++ app/Observers/AgroDealerObserver.php
@@ -2,14 +2,15 @@
 
 namespace App\Observers;
 
-use App\Models\AgroDealer;
+use App\Models\AgroDealers;
+use Illuminate\Support\Facades\DB; 
 
 class AgroDealerObserver
 {
     /**
      * Handle the AgroDealer "created" event.
      */
-    public function created(AgroDealer $agroDealer): void
+    public function created(AgroDealers $agroDealer): void
     {
         //
     }
@@ -17,23 +18,28 @@ class AgroDealerObserver
     /**
      * Handle the AgroDealer "updated" event.
      */
-    public function updated(AgroDealer $agroDealer): void
+    public function updated(AgroDealers $agroDealer): void
     {
         //
     }
 
     /**
-     * Handle the AgroDealer "deleted" event.
+     * Handle the AgroDealer "deleted" event and changes ther users role to basic user and deletes the notifications. 
      */
-    public function deleted(AgroDealer $agroDealer): void
+    public function deleted(AgroDealers $agroDealer): void
     {
-        //
+        DB::table('admin_role_users')
+            ->where('user_id', $agroDealer->user_id)
+            ->update(['role_id' => 3]);
+        DB::table('notifications')
+        ->where('receiver_id', $agroDealer->user_id)
+        ->delete();
     }
 
     /**
      * Handle the AgroDealer "restored" event.
      */
-    public function restored(AgroDealer $agroDealer): void
+    public function restored(AgroDealers $agroDealer): void
     {
         //
     }
@@ -41,7 +47,7 @@ class AgroDealerObserver
     /**
      * Handle the AgroDealer "force deleted" event.
      */
-    public function forceDeleted(AgroDealer $agroDealer): void
+    public function forceDeleted(AgroDealers $agroDealer): void
     {
         //
     }
