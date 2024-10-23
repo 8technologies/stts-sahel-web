03ef0258076c1edd70c65d0245fe9dfdb506ad14
HildahN <98084446+HildahN@users.noreply.github.com>
2024-10-23 16:11:19 +0300

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,2 @@
 import './bootstrap';
-        document.getElementsByClassName("eye-icon").addEventListener("click", function() {
-            var passwordInput = document.getElementById("password-input");
-            var eyeIcon = document.getElementById("eye-icon");
-            if (passwordInput.type === "password") {
-                eyeIcon.classList.remove("fa-eye");
-                eyeIcon.classList.add("fa-eye-slash");
-                passwordInput.type = "text"; // Show password
-                
-            } else {
-                passwordInput.type = "password"; // Hide password
-                eyeIcon.classList.remove("fa-eye-slash");
-                eyeIcon.classList.add("fa-eye");
-            }
-        });
+        
